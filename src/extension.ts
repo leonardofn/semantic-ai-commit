684e2571fd6aa8c01df1f0686825245778cf41c4
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2025-09-02 10:57:23 -0300

    feat: add a command and a new icon to the SCM menu.

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('semantic-ai-commit.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Semantic AI Commit!');
-	});
+	const disposable = vscode.commands.registerCommand(
+    'semantic-ai-commit.generateCommitMessage',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        'Hello World from Semantic AI Commit!'
+      );
+    }
+  );
 
 	context.subscriptions.push(disposable);
 }
